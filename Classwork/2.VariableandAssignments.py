791021c1be662466ae9a83d593f88c88cee8a404
puritipatisrija1516 <puritipatisrija@gmail.com>
2025-07-01 22:33:22 +0530

    variables

---
+++ Classwork/2.VariableandAssignments.py
@@ -0,0 +1,39 @@
+#2.variable Rules And Assignments
+myvar=10
+print(myvar)
+Myvar=10
+print(Myvar)
+myvar1=10
+print(myvar1)
+_myvar=10
+print(_myvar)
+MYvar=10
+print(MYvar)
+#case sensitivity
+myvar=10
+print(myvar)
+myvar=20
+print(myvar)
+#Multiple Assingment
+a,b,c=1,2,3
+print(a)
+print(b)
+print(c)
+#same value Assigment
+a=b=c=10
+print(a,b,c)
+#swapping
+a=1
+b=2
+a,b=b,a
+print(a)
+print(b)
+#deletion of variable
+del a
+del(b)
+a=1
+b=1
+del(a,b)
+
+
+
