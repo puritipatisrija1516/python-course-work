5ac73cace6238dde2490fbf31ae3afc991337a34
puritipatisrija1516 <puritipatisrija@gmail.com>
2025-07-02 11:26:45 +0530

    Arithmetic

---
+++ Classwork/4.PythonOperators.py
@@ -0,0 +1,10 @@
+#1.Arithmetic Operators
+a=40
+b=50
+print("Addition(+):",a+b) #Addition(+): 90
+print("Subtraction(-):",a-b) #Subtraction(-): -10
+print("Multiplication(*):",a*b) #Multiplication(*): 2000
+print("Division(/):",a/b)   #Division(/): 0.8
+print("Modules(%):",a%b) #Modules(%): 40
+print("Floor Division(//):",a//b) #Floor Division(//): 0
+print("explonation(**):",a**b) #explonation(**): 1267650600228229401496703205376000000000000000
